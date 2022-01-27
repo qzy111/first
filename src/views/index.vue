8cf7db775bc7dd638794fb2db33a81cd31fb95e8
qzy <qzy@zlsoft.cn>
2022-01-27 17:35:48 +0800

    调整配置文件

---
+++ src/views/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ code }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js',
+      code: '111'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
